73b5513344d5cc3d338bf93bd6ec48d6f83a6f90
Bsting118 <brendansting@gmail.com>
2023-08-02 12:21:54 -0400

    Added hand animation anims + scripts
    -Couldn't get animations to work at runtime, but hand anim stuff compiles so just putting aside for now.

---
+++ Assets/ExtraCustomAssets/VR/Hands/Hand.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+
+    Animator animator;
+    public float speed;
+    private float gripTarget;
+    private float gripCurrent;
+    private float triggerTarget;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+
+    void AnimateHand()
+    {
+        if (gripTarget != gripCurrent)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+        if (triggerTarget != triggerCurrent)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+    }
+}
