b871b555a9476f8f447138195fed13532f8e18d0
Happyrobot33 <matthewherber@sbcglobal.net>
2023-08-08 08:02:57 -0400

    Add resetting grab position to new cannon object

---
+++ Assets/Scripts/HandleReset.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class HandleReset : MonoBehaviour
+{
+    public Transform ResetPos;
+
+    public void Reset()
+    {
+        transform.position = ResetPos.position;
+        transform.rotation = ResetPos.rotation;
+    }
+}
+
+// add a button in inspector to call the Reset() function
+[CustomEditor(typeof(HandleReset))]
+public class HandleResetEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        HandleReset myScript = (HandleReset)target;
+        if (GUILayout.Button("Reset"))
+        {
+            myScript.Reset();
+        }
+    }
+}
